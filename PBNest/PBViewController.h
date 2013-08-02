bc2f1051e410754c1bf633dc71a2c40e53c92522
Haifisch <laws.ra1n@me.com>
2013-08-01 17:21:32 -0700

    wat

--- PBNest/PBViewController.h
+++ PBNest/PBViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface PBViewController : UIViewController
+#import <PebbleKit/PebbleKit.h>
+@interface PBViewController : UIViewController <UITextFieldDelegate> {
+    NSTimer *wat;
+}
+@property (strong, nonatomic) IBOutlet UITextField *timeInt;
+@property (strong, nonatomic) IBOutlet UITextField *usernameField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordField;
+- (IBAction)updateStart:(id)sender;
+@property (strong, nonatomic) IBOutlet UIButton *updateBtn;
 
 @end
--- PBNest/PBViewController.m
+++ PBNest/PBViewController.m
@@ -8,16 +8,139 @@
 
 #import "PBViewController.h"
 
-@interface PBViewController ()
+@interface PBViewController () <PBPebbleCentralDelegate>
+
 
 @end
 
-@implementation PBViewController
+@implementation PBViewController {
+    PBWatch *_targetWatch;
+}
+
+-(void)refreshAction:(id)sender {
+    if (_targetWatch == nil || [_targetWatch isConnected] == NO) {
+        [[[UIAlertView alloc] initWithTitle:nil message:@"No connected watch!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+       
+        return;
+    }
+    
+    // Fetch weather at current location using openweathermap.org's JSON API:
+    NSString *apiURLString = [NSString stringWithFormat:@"http://pbdb.dylanlaws.com/nest/getTemp.php?email=%@&password=%@", self.usernameField.text, self.passwordField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:apiURLString]];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        NSHTTPURLResponse *httpResponse = nil;
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            httpResponse = (NSHTTPURLResponse *) response;
+        }
+        
+        // NSURLConnection's completionHandler is called on the background thread.
+        // Prepare a block to show an alert on the main thread:
+        __block NSString *message = @"";
+        void (^showAlert)(void) = ^{
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+           //     [[[UIAlertView alloc] initWithTitle:nil message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                 NSLog(@"%@",message);
+            }];
+           
+        };
+        
+        // Check for error or non-OK statusCode:
+        if (error || httpResponse.statusCode != 200) {
+            message = @"Error fetching weather";
+            showAlert();
+            return;
+        }
+        
+        // Parse the JSON response:
+        NSError *jsonError = nil;
+        NSDictionary *root = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        @try {
+            if (jsonError == nil && root) {
+                // TODO: type checking / validation, this is really dangerous...
+                
+                
+                // Get the temperature:
+                NSNumber *temperatureNumber = root[@"1"]; // in degrees Kelvin
+                NSString *formattedTemp = [NSString stringWithFormat:@"%@", temperatureNumber];
+                
+                // Get weather icon:
+                //NSNumber *weatherIconNumber = firstListItem[@"weather"][0][@"icon"];
+                //uint8_t weatherIconID = [self getIconFromWeatherId:[weatherIconNumber integerValue]];
+                
+                // Send data to watch:
+                // See demos/feature_app_messages/weather.c in the native watch app SDK for the same definitions on the watch's end:
+                //NSNumber *iconKey = @(0); // This is our custom-defined key for the icon ID, which is of type uint8_t.
+                NSNumber *temperatureKey = @(1); // This is our custom-defined key for the temperature string.
+                NSDictionary *update = @{
+                                          temperatureKey:[NSString stringWithFormat:@"%@", formattedTemp] };
+                [_targetWatch appMessagesPushUpdate:update onSent:^(PBWatch *watch, NSDictionary *update, NSError *error) {
+                    message = error ? [error localizedDescription] : @"Update sent!";
+                    showAlert();
+                }];
+                return;
+            }
+        }
+        @catch (NSException *exception) {
+        }
+        message = @"Error parsing response";
+        showAlert();
+    }];
+}
+
+- (void)setTargetWatch:(PBWatch*)watch {
+    _targetWatch = watch;
+    
+    // NOTE:
+    // For demonstration purposes, we start communicating with the watch immediately upon connection,
+    // because we are calling -appMessagesGetIsSupported: here, which implicitely opens the communication session.
+    // Real world apps should communicate only if the user is actively using the app, because there
+    // is one communication session that is shared between all 3rd party iOS apps.
+    
+    // Test if the Pebble's firmware supports AppMessages / Weather:
+    [watch appMessagesGetIsSupported:^(PBWatch *watch, BOOL isAppMessagesSupported) {
+        if (isAppMessagesSupported) {
+            // Configure our communications channel to target the weather app:
+            // See demos/feature_app_messages/weather.c in the native watch app SDK for the same definition on the watch's end:
+            uint8_t bytes[] = {0x42, 0xc8, 0x6e, 0xa4, 0x1c, 0x3e, 0x4a, 0x7, 0xb8, 0x89, 0x2c, 0xcc, 0xca, 0x91, 0x41, 0x98};
+            NSData *uuid = [NSData dataWithBytes:bytes length:sizeof(bytes)];
+            [watch appMessagesSetUUID:uuid];
+            
+            NSString *message = [NSString stringWithFormat:@"Yay! %@ supports AppMessages :D", [watch name]];
+            //[[[UIAlertView alloc] initWithTitle:@"Connected!" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+            NSLog(@"%@",message);
+        } else {
+            
+            NSString *message = [NSString stringWithFormat:@"Blegh... %@ does NOT support AppMessages :'(", [watch name]];
+            //[[[UIAlertView alloc] initWithTitle:@"Connected..." message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+            NSLog(@"%@",message);
+        }
+    }];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // We'd like to get called when Pebbles connect and disconnect, so become the delegate of PBPebbleCentral:
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"noisy_grid"]];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if([defaults objectForKey:@"username"] != NULL && [defaults objectForKey:@"password"]){
+        self.usernameField.text = [defaults objectForKey:@"username"];
+        self.passwordField.text = [defaults objectForKey:@"password"];
+        wat = [NSTimer scheduledTimerWithTimeInterval:[self.timeInt.text integerValue]
+                                         target:self
+                                       selector:@selector(refreshAction:)
+                                       userInfo:nil
+                                        repeats:YES];
+        }else {
+       [[[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Please fill in your user data, then click the button!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }
+    [[PBPebbleCentral defaultCentral] setDelegate:self];
+    
+    // Initialize with the last connected watch:
+    [self setTargetWatch:[[PBPebbleCentral defaultCentral] lastConnectedWatch]];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +148,47 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+/*
+ *  PBPebbleCentral delegate methods
+ */
 
+- (void)pebbleCentral:(PBPebbleCentral*)central watchDidConnect:(PBWatch*)watch isNew:(BOOL)isNew {
+    [self setTargetWatch:watch];
+    wat = [NSTimer scheduledTimerWithTimeInterval:[self.timeInt.text integerValue]
+                                           target:self
+                                         selector:@selector(refreshAction:)
+                                         userInfo:nil
+                                          repeats:YES];
+}
+
+- (void)pebbleCentral:(PBPebbleCentral*)central watchDidDisconnect:(PBWatch*)watch {
+    [wat invalidate];
+    [[[UIAlertView alloc] initWithTitle:@"Disconnected!" message:[watch name] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    if (_targetWatch == watch || [watch isEqual:_targetWatch]) {
+        [self setTargetWatch:nil];
+    }
+    [watch wake];
+}
+-(BOOL) textFieldShouldReturn:(UITextField *)textField{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:self.usernameField.text forKey:@"username"];
+    [defaults setObject:self.passwordField.text forKey:@"password"];
+    [defaults synchronize];
+    [textField resignFirstResponder];
+    return YES;
+}
+- (IBAction)updateStart:(id)sender {
+    NSLog(@"stoping");
+    [wat invalidate];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:self.usernameField.text forKey:@"username"];
+    [defaults setObject:self.passwordField.text forKey:@"password"];
+    [defaults synchronize];
+    wat = [NSTimer scheduledTimerWithTimeInterval:[self.timeInt.text integerValue]
+                                           target:self
+                                         selector:@selector(refreshAction:)
+                                         userInfo:nil
+                                          repeats:YES];
+    NSLog(@"starting");
+}
 @end
